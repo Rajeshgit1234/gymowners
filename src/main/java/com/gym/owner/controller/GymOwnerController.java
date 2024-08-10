a9e7d0919c8a81177bf2ffb75fdd20061d311534
rajeshrajan <rajeshawsconnect@gmail.com>
2024-08-10 13:35:27 +0530

    db changes

--- src/main/java/com/gym/owner/controller/GymOwnerController.java
+++ src/main/java/com/gym/owner/controller/GymOwnerController.java
@@ -104,27 +104,57 @@ public class GymOwnerController {
 
 
     }
-
+    @CrossOrigin
     @PostMapping("/addExpense")
     public String addExpense(@RequestBody String jsonReq) {
 
         Boolean status = false;
         String statusDesc = "Failed";
-        JSONArray expenseList =new JSONArray();
-
+        JSONArray expenseJson =new JSONArray();
+        DecimalFormat formatter
+                = new DecimalFormat("₹#,##0.00");
         JSONObject res = new JSONObject();
         try{
             System.out.println("gymOwnerService --> addExpense "+jsonReq);
             JSONObject req = new JSONObject(jsonReq);
+            String gym_id_str = req.get("gym_id").toString();
+            String user_id = req.get("user_id").toString();
+            String expDate = req.get("expDate").toString();
+            String exp_id = req.get("exp_id").toString();
+            String exp_remarks = req.get("exp_remarks").toString();
+            String exp_amount = req.get("amount").toString();
 
             statusDesc = "Operation failed";
 
+
+            int gym_id = Integer.valueOf(gym_id_str);
+            List<Map<String, Object>> expenseList= gymExpenseListService.getGymExpenseListQuery(gym_id);
+
+            if(!expenseList.isEmpty()){
+
+                for(Map<String, Object> expense:expenseList){
+                    JSONObject expenseItem = new JSONObject();
+                    Object amount = ((expense.get("amount") == null) ? "0.00" : expense.get("amount"));
+                    BigDecimal amt = new BigDecimal(amount.toString());
+                    String currStr = formatter.format(amt);
+
+                    expenseItem.put("exp_id",expense.get("id"));
+                    expenseItem.put("created_by",((expense.get("name") == null) ? "N/A" : expense.get("name")));
+                    expenseItem.put("created_on",((expense.get("created_on") == null) ? "N/A" : expense.get("created_on")));
+                    expenseItem.put("expense_item",((expense.get("expense_item") == null) ? "N/A" : expense.get("expense_item")));
+                    expenseItem.put("exp_remarks",((expense.get("exp_remarks") == null) ? "N/A" : expense.get("exp_remarks")));
+                    expenseItem.put("exp_amount",currStr);
+
+                    expenseJson.put(expenseItem);
+                }
+
+            }
 
 
         }catch(Exception e){ e.printStackTrace();}finally {
             res.put("status",status);
             res.put("statusDesc",statusDesc);
-            res.put("expenseList",expenseList );
+            res.put("expenseList",expenseJson );
         }
         return res.toString();
 
@@ -142,7 +172,7 @@ public class GymOwnerController {
 
         JSONArray expenseJson =new JSONArray();
         DecimalFormat formatter
-                = new DecimalFormat("$#,##0.00");
+                = new DecimalFormat("₹#,##0.00");
         JSONObject res = new JSONObject();
         try{
             System.out.println("gymOwnerService --> addExpense "+jsonReq);
